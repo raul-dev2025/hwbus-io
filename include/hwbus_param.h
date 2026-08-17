604ee89d76bd8553168f9749950c36979c5b488c
Raul Vilchez <raulmicrosistemas@gmail.com>
2026-08-17 17:39:44 +0200

    feat(driver): declare hwbus_get_pci_dev_from_param helper
    - Add function prototype to retrieve pci_dev instance using BDF parameter.

--- include/hwbus_param.h
+++ include/hwbus_param.h
@@ -6,4 +6,7 @@
 /*Parametros expuestos*/
 extern char *hwbus_bdf_param;
 
+/* Helper para obtener el pci_dev actual a partir del parámetro BDF */
+struct pci_dev *hwbus_get_pci_dev_from_param(void);
+
 #endif // HWBUS_PARAM_H
