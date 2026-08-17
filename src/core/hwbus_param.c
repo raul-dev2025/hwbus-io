0bbb4d989a5e2e5b46a3d1485dacb2c9d25c7971
Raul Vilchez <raulmicrosistemas@gmail.com>
2026-08-17 17:43:55 +0200

    feat(driver): add hwbus_param.c for module parameter handling
    - Create core source file with kernel module headers and license declaration.

---
+++ src/core/hwbus_param.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * hwbus_param.c - Parámetros disponibles
+ *
+ * Copyright (C) 2026 Raúl Vílchez Ruiz <raulmicrosistemas@gmail.com>
+ */
+
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/pci.h>
+#include "hwbus_param.h"
+
