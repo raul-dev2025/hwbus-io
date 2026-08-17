7bce95b2923d1d00f76632ea365256c4c72c1459
Raul Vilchez <raulmicrosistemas@gmail.com>
2026-08-17 17:47:32 +0200

    feat(driver): add bdf module parameter in hwbus_param.c
    - Define hwbus_bdf_param with default value "0000:02:00.0" and expose read-only module parameter.

--- src/core/hwbus_param.c
+++ src/core/hwbus_param.c
@@ -10,3 +10,8 @@
 #include <linux/pci.h>
 #include "hwbus_param.h"
 
+// Valor por defecto: "0000:02:00.0"
+char *hwbus_bdf_param = "0000:02:00.0";
+module_param_named(bdf, hwbus_bdf_param, charp, 0444);
+MODULE_PARM_DESC(bdf, "Target PCI Device BDF (e.g. 0000:02:00.0)");
+
