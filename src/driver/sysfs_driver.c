7cfefccca89a0b6d477f9f3d1e9ad4ff651a350c
Raul Vilchez <raulmicrosistemas@gmail.com>
2026-08-17 15:43:07 +0200

    feat(driver): add initial sysfs_driver.c file for PCI attribute access

---
+++ src/driver/sysfs_driver.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * sysfs_driver.c - single device pci attribute reading
+ *
+ * Copyright (C) 2026 Raúl Vílchez Ruiz <raulmicrosistemas@gmail.com>
+ */
+#include <linux/pci.h>
