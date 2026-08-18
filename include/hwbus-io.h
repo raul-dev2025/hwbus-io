81bc3905cb08ed0d83fb898a6291b102f26bdece
Raul Vilchez <raulmicrosistemas@gmail.com>
2026-08-18 16:16:56 +0200

    docs(include): add pdev field documentation and member to hwbus_dev struct
    - Add pdev pointer to hwbus_dev structure to store the associated PCI device context and document its usage in the header comment.

--- include/hwbus-io.h
+++ include/hwbus-io.h
@@ -18,12 +18,15 @@
  * struct hwbus_dev - Estructura principal de contexto del driver hwbus-io
  * @dev_id: Identificador único interno
  * @is_active: Estado de inicialización del dispositivo
+ * @cdev: dispositivo de carácteres asociado
+ * @pdev: dispositivo PCI asociado(BDF)
  */
 struct hwbus_dev
 {
   u32 dev_id;
   bool is_active;
   struct cdev cdev;
+  struct pci_dev *pdev;
 };
 
 int __init hwbus_init(void);
