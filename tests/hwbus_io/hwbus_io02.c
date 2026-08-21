0de6af99d25a757d1166f223a13c948d9e7cc0dd
Raul Vilchez <raulmicrosistemas@gmail.com>
2026-08-21 11:48:59 +0200

    test: Add new test cases (hwbus_io02 to hwbus_io05) to staged changes

---
+++ tests/hwbus_io/hwbus_io02.c
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * hwbus_io02.c - Validación de Lecturas y Contenido PCI
+ *
+ * Copyright (C) 2026 Raúl Vílchez Ruiz <raulmicrosistemas@gmail.com>
+ */
---
+++ tests/hwbus_io/hwbus_io03.c
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * hwbus_io03.c - Validación de Restricción de Escritura
+ *
+ * Copyright (C) 2026 Raúl Vílchez Ruiz <raulmicrosistemas@gmail.com>
+ */
