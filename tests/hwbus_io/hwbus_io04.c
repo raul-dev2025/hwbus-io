ec498648ffdc743c99e0fae0c3a9d706e0ca7917
Raul Vilchez <raulmicrosistemas@gmail.com>
2026-08-31 18:10:56 +0200

    tests: add initial skeleton for module parameter test in hwbus_io04.c
    - Include LTP test framework headers, define file paths and setup/cleanup
    functions, and register root/kconfig test requirements in tst_test structure.

--- tests/hwbus_io/hwbus_io04.c
+++ tests/hwbus_io/hwbus_io04.c
@@ -4,3 +4,46 @@
  *
  * Copyright (C) 2026 Raúl Vílchez Ruiz <raulmicrosistemas@gmail.com>
  */
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "tst_test.h"
+#include "tst_module.h"
+#include <user/hwbus_io_uapi.h>
+
+#define DEV_PATH "/dev/hwbusc"
+#define BDF_PARAM "/sys/module/hwbus_io/parameters/bdf"
+#define MODULE_PATH "/mnt/build-output/Repos/hwbus-io.git/src"
+
+static int fd = -1;
+
+
+static void setup(void)
+{
+  fd = SAFE_OPEN(DEV_PATH, O_RDWR);
+}
+
+static void cleanup(void)
+{
+  if (fd >= 0)
+    SAFE_CLOSE(fd);
+}
+
+static void run_test(void)
+{
+}
+
+static struct tst_test test = {
+    .needs_root = 1,
+    .needs_kconfigs = 1,
+    .setup = setup,
+    .cleanup = cleanup,
+    .test_all = run_test,
+};
