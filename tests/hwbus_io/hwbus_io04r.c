14f72eeb44197de8a40da501e3b3b4cd890c134d
Raul Vilchez <raulmicrosistemas@gmail.com>
2026-09-03 11:16:36 +0200

    test: restore multi-device BDF array in hwbus_io04r test

--- tests/hwbus_io/hwbus_io04r.c
+++ tests/hwbus_io/hwbus_io04r.c
@@ -127,8 +127,8 @@ static void cleanup(void)
 
 static void run_test(void)
 {
-  // const char *test_bdfs[] = {"0000:03:00.0", "0000:00:02.2", "0000:0a:01.0"};
-  const char *test_bdfs[] = {"0000:03:00.0"};
+  const char *test_bdfs[] = {"0000:03:00.0", "0000:00:02.2", "0000:0a:01.0"};
+  // const char *test_bdfs[] = {"0000:03:00.0"};
   for (int i = 0; i < 1; i++)
   {
     reload_hwbus_module(test_bdfs[i]);
