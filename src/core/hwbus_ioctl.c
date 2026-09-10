8aa3e0e979f4a5be4ccc1d22955b03eab2148be3
Raul Vilchez <raulmicrosistemas@gmail.com>
2026-09-10 15:47:40 +0200

    refactor(ioctl): comment out debug log in unlocked_ioctl handler

--- src/core/hwbus_ioctl.c
+++ src/core/hwbus_ioctl.c
@@ -57,7 +57,7 @@ long hwbus_unlocked_ioctl(struct file *filp, unsigned int cmd, unsigned long arg
 
   int ret;
 
-  pr_info("hwbus ioctl cmd recibido: 0x%x, esperado RESET: 0x%lx\n", cmd, (unsigned long)HWBUS_IOCRESET);
+  // pr_info("hwbus ioctl cmd recibido: 0x%x, esperado RESET: 0x%lx\n", cmd, (unsigned long)HWBUS_IOCRESET);
 
   switch (cmd)
   {
